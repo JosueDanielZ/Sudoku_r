86f4919071fe07528329e742b73542fd61cfa821
Tortrixxgd <zamoradani087@gmail.com>
2022-05-02 21:57:21 -0600

    creando tablero

--- logicasudoku.py
+++ logicasudoku.py
@@ -1,1 +1,22 @@
-print ("Hola")
+import numpy as np
+class Cuerpo():
+    def __init__(self):
+        self.cuerpo = np.array([[]])
+
+    def iniciarcuerpo(self):
+        self.cuerpo = np.array([[0,0,0,0,0,0,0,0,0],
+                                [0,0,0,0,0,0,0,0,0],
+                                [0,0,0,0,0,0,0,0,0],
+                                [0,0,0,0,0,0,0,0,0],
+                                [0,0,0,0,0,0,0,0,0],
+                                [0,0,0,0,0,0,0,0,0],
+                                [0,0,0,0,0,0,0,0,0],
+                                [0,0,0,0,0,0,0,0,0],
+                                [0,0,0,0,0,0,0,0,0]])
+
+    def imprimirtabla(self):
+        print(self.cuerpo)
+
+cuerposu = Cuerpo()
+cuerposu.iniciarcuerpo()
+cuerposu.imprimirtabla()
